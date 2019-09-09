456630b50011e8c8badeef091cc934cf040e8b67
tbrunetti <tonya.brunetti@colorado.edu>
2019-09-09 17:17:39 -0600

    updated logger handles

--- generate_LD.py
+++ generate_LD.py
@@ -10,7 +10,9 @@ import subprocess
 @profile(precision=4)
 def prepData(genoFile, genoFileType, inputTable, chrm, LDrange, outdir, config):
 	logger = logging.getLogger('prepData')
-	logging.debug('Running function prepData()')
+	logger.setLevel(logging.DEBUG)
+
+	logger.debug('Running function prepData()')
 
 	allAssocData = pandas.read_csv(inputTable, usecols = ['CHROM', 'BEGIN', 'END', 'PVALUE'], skipinitialspace = True, sep='\t', 
 		dtype={'CHROM':'str', 'BEGIN':'int64', 'END':'int64', 'PVALUE':'float64'})
@@ -18,7 +20,7 @@ def prepData(genoFile, genoFileType, inputTable, chrm, LDrange, outdir, config):
 	tableSubset = allAssocData.loc[(allAssocData['CHROM'] == chrm) & (allAssocData['BEGIN'] >= int(LDrange.split('-')[0].rstrip()))
 					& (allAssocData['END'] <= int(LDrange.split('-')[1].rstrip()))]
 	tableSubset['MARKER_ID'] = tableSubset['CHROM'].astype(str) + ':' +tableSubset['BEGIN'].astype(str)
-	logging.info('Finshed subsetting summary statistics data...preparing to clear memory to free RAM')
+	logger.debug('Finshed subsetting summary statistics data...preparing to clear memory to free RAM')
 	
 	
 	# START FREE MEMORY OF LARGE DATAFRAME
@@ -33,7 +35,7 @@ def prepData(genoFile, genoFileType, inputTable, chrm, LDrange, outdir, config):
 	if genoFileType == 'vcf':
 		pass;
 	elif genoFileType == 'plink':
-		logging.debug('Subsetting plink file to match tableSubet')
+		logger.debug('Subsetting plink file to match tableSubet')
 		subprocess.call([config['plink'],
 			'--bfile', genoFile[:-4],
 			'--chr', chrm,
@@ -46,14 +48,13 @@ def prepData(genoFile, genoFileType, inputTable, chrm, LDrange, outdir, config):
 	try:
 		assert os.path.isfile(os.path.join(outdir, '{}_{}_{}.bed'.format(genoFile.split('/')[-1][:-4], LDrange.split('-')[0].rstrip(), LDrange.split('-')[1].rstrip())))
 	except AssertionError:
-		logging.critical('Unable to generate plink file {}_{}_{}.bed.  System Exiting'.format(genoFile.split('/')[-1][:-4], LDrange.split('-')[0].rstrip(), LDrange.split('-')[1].rstrip()))
+		logger.critical('Unable to generate plink file {}_{}_{}.bed.  System Exiting'.format(genoFile.split('/')[-1][:-4], LDrange.split('-')[0].rstrip(), LDrange.split('-')[1].rstrip()))
 		print('Critical Error Reached!  Unable to subset data from plink file.  System Exiting')
 		sys.exit()
 
-	logging.info('Writing results to {}'.format(os.path.join(outdir, 'subsetData_{}_{}_{}.txt'.format(genoFile.split('/')[-1][:-4], LDrange.split('-')[0].rstrip(), LDrange.split('-')[1].rstrip()))))
+	logger.info('Writing results to {}'.format(os.path.join(outdir, 'subsetData_{}_{}_{}.txt'.format(genoFile.split('/')[-1][:-4], LDrange.split('-')[0].rstrip(), LDrange.split('-')[1].rstrip()))))
 	tableSubset.to_csv(os.path.join(outdir, 'subsetData_{}_{}_{}.txt'.format(genoFile.split('/')[-1][:-4], LDrange.split('-')[0].rstrip(), LDrange.split('-')[1].rstrip())),
 			index=False, sep='\t')
-
 
 	findRefSnp(tableSubset=tableSubset, data=os.path.join(outdir, '{}_{}_{}'.format(genoFile.split('/')[-1][:-4], LDrange.split('-')[0].rstrip(), LDrange.split('-')[1].rstrip())), outdir=outdir)
 
@@ -61,12 +62,13 @@ def prepData(genoFile, genoFileType, inputTable, chrm, LDrange, outdir, config):
 @profile(precision=4)
 def findRefSnp(tableSubset, data, outdir):
 	logger = logging.getLogger('findRefSnp')
-	logging.debug('Running function findRefSnp()')
+	logger.setLevel(logging.DEBUG)
+	logger.debug('Running function findRefSnp()')
 
 	refSnp = tableSubset.loc[tableSubset['PVALUE'].idxmin()]['MARKER_ID']
 	pval = tableSubset.loc[tableSubset['PVALUE'].idxmin()]['PVALUE']
 	print('The reference SNP for LD in this subset is {} and has a p-value of {}'.format(refSnp, pval))
-	logging.info('The reference SNP for LD in this subset is {} and has a p-value of {}'.format(refSnp, pval))
+	logger.info('The reference SNP for LD in this subset is {} and has a p-value of {}'.format(refSnp, pval))
 
 	# TO DO: if refSNP is more than one in same position take the first index (doesn't matter because they will both be in LD)
 
@@ -78,7 +80,8 @@ def findRefSnp(tableSubset, data, outdir):
 @profile(precision=4)
 def calculateLD(data, refSnp, outdir, config):
 	logger = logging.getLogger('calculateLD')
-	logging.debug('Running function calculateLD()')
+	logger.setLevel(logging.DEBUG)
+	logger.debug('Running function calculateLD()')
 
 	subprocess.call([
 		config['plink'],
@@ -92,22 +95,27 @@ def calculateLD(data, refSnp, outdir, config):
 		]
 	)
 
+	logger.debug("Finished generating LD file from plink")
 
-	#TODO: change header names to match expectation of LZ
+
+	#change header names to match expectation of LZ
+	logger.debug("Updating headers to match locuszoom expected input...")
 	plinkLD = pandas.read_csv(os.path.join(outdir, '{}_plinkCalculatedLD.ld'.format(data)), usecols=['SNP_B', 'SNP_A', 'DP', 'R2'], skipinitialspace=True,  delim_whitespace=True)
 
 	lzLDformat = plinkLD.rename(columns={'SNP_B':'snp1', 'SNP_A':'snp2', 'DP':'dprime', 'R2':'rsquare'})
 
 	lzLDformat.to_csv(os.path.join(outdir, '{}_plinkCalculatedLD_convertedForLZinput.ld'.format(data)), sep="\t", index=False)
-	
+	logger.debug("Finsihed updating headers for locuszoom compatability")
 
+	#TODO: call runLZ()
 
 @profile(precision=4)
 def runLZ(outdir, config):
 	logger = logging.getLogger('runLZ')
-	logging.debug('Running function runLZ()')
+	logger.setLevel(logging.DEBUG)
+	logger.debug('Running function runLZ()')
 
-	pass;
+
 
 
 if __name__ == '__main__':
@@ -132,18 +140,19 @@ if __name__ == '__main__':
 
 	logging.basicConfig(filename='generateLD.log', level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 	logger = logging.getLogger('main_runtime')
+	logger.setLevel(logging.DEBUG)
 
-	logging.debug('Checking table format and headers')
+	logger.debug('Checking table format and headers')
 	
 	try:
 		readTableHeader = open(args.inputTable, 'r')
 		header = readTableHeader.readline().rstrip().split('\t')
 		for names in ['CHROM', 'BEGIN', 'END', 'PVALUE']:
 			assert names in header
-			logging.info('confirmed, {} found in header of input table'.format(names))
+			logger.info('confirmed, {} found in header of input table'.format(names))
 	
 	except AssertionError:
-		logging.critical('FAIL, {} not found in header of input table; Program exiting.'.format(names))
+		logger.critical('FAIL, {} not found in header of input table; Program exiting.'.format(names))
 		sys.exit(42)
 	
 	# read and store config file as dictionary
@@ -156,16 +165,16 @@ if __name__ == '__main__':
 	try:
 		assert os.path.exists(args.outdir)
 	except AssertionError:
-		logging.info('{} does not exist, creating new directory...'.format(args.outdir))
+		logger.info('{} does not exist, creating new directory...'.format(args.outdir))
 		os.mkdir(args.outdir)
 
 
 
 	if args.vcfGeno != None:
-		logging.info("Input genotype file is dectected as VCF")
+		logger.info("Input genotype file is dectected as VCF")
 		prepData(genoFile=args.vcfGeno, genoFileType='vcf', inputTable=args.inputTable, chrm=args.chrm, LDrange=args.LDrange, outdir=args.outdir, config=config)
 	elif args.plinkGeno != None:
-		logging.info("Input genotype file is detected as plink format")
+		logger.info("Input genotype file is detected as plink format")
 		prepData(genoFile=args.plinkGeno, genoFileType='plink', inputTable=args.inputTable, chrm=args.chrm, LDrange=args.LDrange, outdir=args.outdir, config=config)
 	else:
 		pass

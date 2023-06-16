048205d43810c9ea6920a46ee42d7cdbd568efe9
tbrunetti <tonya.brunetti@colorado.edu>
2023-06-15 22:03:11 -0600

    annotation updates

--- gtf_manipulation_functions.py
+++ gtf_manipulation_functions.py
@@ -19,8 +19,8 @@ def add_transcript(gtf:str, out:str) -> None:
                 transcript_id = match.group(1).split("\"")[1]
                 strand = line.split('\t')[6]
                 chromosome = line.split('\t')[0]
-                start_exon = line.split('\t')[3]
-                end_exon = line.split('\t')[4]
+                start_exon = int(line.split('\t')[3])
+                end_exon = int(line.split('\t')[4])
                 ref_db = line.split('\t')[1]
                 if transcript_id in transcript_data:
                     if start_exon < transcript_data[transcript_id]['start_exon']:
@@ -43,8 +43,8 @@ def add_transcript(gtf:str, out:str) -> None:
         for transcripts in transcript_data:
             gtf_write.write(transcript_data[transcripts]['chromosome'] +
             '\t' + transcript_data[transcripts]['ref_db'] + 
-            '\ttranscript\t' + transcript_data[transcripts]['start_exon'] +
-            '\t' + transcript_data[transcripts]['end_exon'] + '\t.\t' +
+            '\ttranscript\t' + str(transcript_data[transcripts]['start_exon']) +
+            '\t' + str(transcript_data[transcripts]['end_exon']) + '\t.\t' +
             transcript_data[transcripts]['strand'] + '\t.\t' + 
             transcript_data[transcripts]['metadata'] + '\n'
             )
